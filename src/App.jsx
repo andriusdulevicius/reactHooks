d1f34af5cea28e49cb0a57ac8267cf198c240732
AndriusDulevicius <andriusdulevicius@email.com>
2021-08-09 09:23:05 +0300

    counter works with hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css';
+import Counter from './components/Counter';
 
 function App() {
   return (
     <div className='App'>
       <h1>React Hooks intro</h1>
+      <Counter />
     </div>
   );
 }
